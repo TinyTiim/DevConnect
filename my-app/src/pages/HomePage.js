1acebc16e02ae89e0af08c480e925462c77b48b3
Ani-Kandikattu <anikandikattu@gmail.com>
2023-06-30 10:21:38 -0700

    modified App.js slightly and started working on HomePage.js, also added HomePage.css

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,5 +1,15 @@
-const HomePage = () => {
-  return <div>Home Page</div>;
+import React from 'react';
+import "./HomePage.css";
+
+function HomePage() { 
+  <div className = "Home">
+    <div className='devConnect'>
+      <div className='devConnecttext'>
+        Home
+      </div>
+    </div>  
+  </div>;
 };
+
 
 export default HomePage;
