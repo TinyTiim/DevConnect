7e21f0d078f91f4c9a975c74dcd391e7e627a7b2
Irrumabo <ballenstev@gmail.com>
2023-07-05 19:32:25 -0700

    added resolvers

--- my-app/server/schemas/resolvers.js
+++ my-app/server/schemas/resolvers.js
@@ -1,13 +1,59 @@
 // add resolver logic
 const { Profile } = require('../models');
-// add in authenticators to the code
+const { errInAuth } = require('apollo-server-express');
+const { signToken } = require('../../client/src/utils/auth');
 
 const resolvers = {
     // add resolvers for the query and mutations
+    Query: {
+        profiles: async () => {
+            return Profile.find();
+        },
+        profile: async (parent, { profileId }) => {
+            return Profile.findOne({ _id: profileId});
+        },
+        me: async (parent, args, context) => {
+            if (context.user){
+                return Profile.findOne({ _id: context.user._id });
+            };
+            throw new errInAuth('Please log-in to attempt this action!');
+        },
+    },
 
+    Mutation: {
+       //add mutation for creating a new user
+       addBio: async (parent, {username, bio}, context) => {
+        if (context.user) {
+            return Profile.findOneAndUpdate(
+                {_id: username},
+                {
+                    $addToSet: {biography: bio},
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                },
+            );
+        }
+        throw new errInAuth('Please log-in to attempt this action!');
+       },
+       deleteProfile: async (parent, args, context) => {
+        if(context.user) {
+            return Profile.findOneAndDelete({ _id: context.user._id});
+        }
+        throw new errInAuth('Please log-in to attempt this action!');
+       },
+       deleteBio: async (parent, {bio}, context) => {
+        if(context.user) {
+            return Profile.findOneAndDelete(
+                { _id: context.user._id },
+                { $pull: {biography, bio} },
+                {new: true},
+            );
+        }
+        throw new errInAuth('Please log-in to attempt this action!');
+       },
+    },
 };
-
-// maybe add in the ability to add or remove stuff from the profiles
-
 
 module.exports = resolvers;
