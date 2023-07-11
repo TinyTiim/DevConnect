b6d114fd710be7f24ca562a87cf3b28dfa3da5cc
Irrumabo <ballenstev@gmail.com>
2023-07-10 19:05:58 -0700

    forgot to add the mongodb connection

--- my-app/server/config/connection.js
+++ my-app/server/config/connection.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DevConnect',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+module.exports = mongoose.connection;
