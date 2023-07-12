1fa547a2cbd0a971630188bfb125c251bf1dad43
unknown <sophia.palomo12@gmail.com>
2023-07-11 23:53:12 -0700

    made the profile able to save so when you switch pages and refresh it will save.

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const mongoose = require('./config/connection');
 const cors = require("cors");
 const { default: axios } = require("axios");
+const UserProfile = require('./models/Profile');
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
@@ -21,6 +24,37 @@ app.post("/authenticate", async (req, res) => {
         return res.status(e.response.status).json(e.response.data);
     }
 });
+
+app.get('/api/userProfile/:username', async (req, res) => {
+    const username = req.params.username;
+  
+    try {
+      const userProfile = await UserProfile.findOne({ username });
+      if (userProfile) {
+        res.status(200).json(userProfile);
+      } else {
+        res.status(404).json({ error: 'User profile not found' });
+      }
+    } catch (error) {
+      console.error('Error fetching user profile:', error);
+      res.status(500).json({ error: 'Failed to fetch user profile' });
+    }
+  });
+  
+  app.post('/api/updateProfile/:username', async (req, res) => {
+    const username = req.params.username;
+    const updatedProfile = req.body;
+  
+    try {
+      await UserProfile.findOneAndUpdate({ username }, updatedProfile, {
+        upsert: true,
+      });
+      res.sendStatus(200);
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      res.status(500).json({ error: 'Failed to update profile' });
+    }
+  });
 
 //project id 
 // 52a95cce-d78f-4c16-ac34-c48d03d414f4
