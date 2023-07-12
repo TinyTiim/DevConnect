19b91fd5baeef415d3ea8a49bdb10ec1b340446f
Ani-Kandikattu <anikandikattu@gmail.com>
2023-07-12 10:36:46 -0700

    finished posts for now, will need to add images and icons

--- my-app/client/src/components/feed/Feed.js
+++ my-app/client/src/components/feed/Feed.js
@@ -8,6 +8,11 @@ export default function Feed() {
       <div className="feedWrapper">
         <Post />
         <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
       </div>
     </div>
   )
