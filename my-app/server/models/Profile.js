eab2ca072eef9941dddf254c72711a29e363b98e
unknown <sophia.palomo12@gmail.com>
2023-07-12 21:27:33 -0700

    cleaned up code and added comments

--- my-app/server/models/Profile.js
+++ my-app/server/models/Profile.js
@@ -1,12 +1,19 @@
+
 const mongoose = require('mongoose');
 
+//Profile Schema
 const userProfileSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+  },
   bio: String,
   avatar: String,
   title: String,
 });
 
+//Creates the profile model
 const UserProfile = mongoose.model('UserProfile', userProfileSchema);
 
+//Exports the profile model
 module.exports = UserProfile;
